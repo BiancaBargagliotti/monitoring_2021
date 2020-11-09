7e075c0e2299c18ebd9500d61b5b7c66997eafcc
BiancaBargagliotti <73693975+BiancaBargagliotti@users.noreply.github.com>
2020-11-09 10:45:09 +0100

    Update R_code_plot_eco_var.r

--- R_code_plot_eco_var.r
+++ R_code_plot_eco_var.r
@@ -38,3 +38,34 @@ pairs(meuse)
 
 # Exercise: pair only the element part of the dataset
 # only cadmium, copper, lead, zinc...
+
+###################
+
+# recall library sp and data meuse
+library(sp)
+data(meuse)
+pairs(meuse)
+head(meuse)
+
+
+# Exercise: pair only the element part of the dataset
+# only cadmium, copper, lead, zinc...
+#from column 3 to column 6
+pairs(meuse[,3:6])
+
+# or how to use the names of the columns
+pairs(~ cadmium + copper + lead + zinc, data=meuse)
+
+# exercise: just use cadmium, lead, zinc
+pairs(~ cadmium + lead + zinc, data=meuse)
+
+#exercise: change the colour
+pairs(~ cadmium + copper + lead + zinc, data=meuse, col="red")
+#code for different colours, 
+pairs(~ cadmium + copper + lead + zinc, data=meuse, col="#669966")
+
+#exercise: change the symbols to filled triangle
+pairs(~ cadmium + copper + lead + zinc, data=meuse, col="#669966", pch=17)
+
+#exercise: increase the size of filled triangle
+pairs(~ cadmium + copper + lead + zinc, data=meuse, col="#669966", pch=17, cex=3)
