8d03235f4e0b45643ffa6cded7ddece85249a4fa
BiancaBargagliotti <73693975+BiancaBargagliotti@users.noreply.github.com>
2020-11-23 10:56:10 +0100

    Update R_code_point_pattern_analysis.r

--- R_code_point_pattern_analysis.r
+++ R_code_point_pattern_analysis.r
@@ -25,3 +25,42 @@ points(covid_planar)
 
 # Putting the countries ontop of the map
 install.packages("rgdal")
+library(rgdal)
+
+# read new files (downloaded and put into the lab folder) with rgdal
+coastlines <- readOGR("ne_10m_coastline.shp")
+
+plot(density_map, col=cl)
+points(covid_planar)
+plot(coastlines, add = TRUE)
+
+#change colours:
+cl <- colorRampPalette(c("pink", "green", "blue", "red")) (100)
+plot(density_map, col=cl)
+
+# change the type of points
+points(covid_planar, pch=19, cex=0.5)
+plot(coastlines, add = TRUE)
+
+# how to save graphs as png and pdf:
+png("figure1.png") 
+cl <- colorRampPalette(c("pink", "green", "blue", "red")) (100)
+plot(density_map, col=cl)
+points(covid_planar, pch=19, cex=0.5)
+plot(coastlines, add = TRUE)
+dev.off()
+
+pdf("figure1.pdf") 
+cl <- colorRampPalette(c("pink", "green", "blue", "red")) (100)
+plot(density_map, col=cl)
+points(covid_planar, pch=19, cex=0.5)
+plot(coastlines, add = TRUE)
+dev.off()
+
+# interpolate cases data
+marks(covid_planar) <- cases
+cases_map <- Smooth(covid_planar)
+plot(cases_map, col=cl)
+plot(coastlines, add = TRUE)
+points(covid_planar, pch=19, cex=0.5)
+
