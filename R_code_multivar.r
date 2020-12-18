5847e58f3f27cf070c164c360dda8626dc6b9413
BiancaBargagliotti <73693975+BiancaBargagliotti@users.noreply.github.com>
2020-12-18 10:49:41 +0100

    Update R_code_multivar.r

--- R_code_multivar.r
+++ R_code_multivar.r
@@ -1,5 +1,43 @@
 # R_code_multivar.r
 
-#install vegan package and recall it
+# install vegan package and recall it
 install.packages("vegan")
 library(vegan)
+
+# set the working directory
+setwd("/Users/bianca/Desktop/GCE&SDGs/Monitoring Ecosystems/lab/")
+
+#load the chosen file
+load("biomes_multivar.RData")
+#to see the list of files 
+ls()
+
+# plot per species matrix
+head(biomes)
+
+# see the correspondences among different species
+multivar <- decorana(biomes) 
+#recall it
+multivar
+
+#plot 
+plot(multivar)
+# biomes types 
+attach(biomes_types)
+# biomes names in the graph
+ordiellipse(multivar, type, col=c("black", "red", "green", "blue"), kind="ehull", lwd=3)
+ordispider(multivar, type, col=c("black","red","green","blue"), label = T)
+
+pdf("biomes.multivar.pdf")
+multivar <- decorana(biomes) 
+#recall it
+multivar
+
+#plot 
+plot(multivar)
+# biomes types 
+attach(biomes_types)
+# biomes names in the graph
+ordiellipse(multivar, type, col=c("black", "red", "green", "blue"), kind="ehull", lwd=3)
+ordispider(multivar, type, col=c("black","red","green","blue"), label = T)
+dev.off()
