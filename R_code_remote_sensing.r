811595e4364de20152d252b61b3f3a02086cfb0a
BiancaBargagliotti <73693975+BiancaBargagliotti@users.noreply.github.com>
2021-01-08 11:07:11 +0100

    Update R_code_remote_sensing.r

--- R_code_remote_sensing.r
+++ R_code_remote_sensing.r
@@ -13,3 +13,48 @@ setwd("/Users/bianca/Desktop/GCE&SDGs/Monitoring Ecosystems/lab/")
 # upload data (after unzipping the files directly in the lab folder)
 p224r63_2011 <- brick("p224r63_2011_masked.grd")
 
+plot(p224r63_2011)
+
+# B1: blue
+# B2: green
+# B3: red
+# B4: NIR
+
+cl <- colorRampPalette(c('black','grey','light grey'))(100) # 
+plot(p224r63_2011, col=cl)
+
+par(mfrow=c(2,2))
+
+#band n1
+clb <- colorRampPalette(c('dark blue','blue','light blue'))(100) # 
+plot(p224r63_2011$B1_sre, col=clb)
+
+#band n2
+clg <- colorRampPalette(c('dark green','green','light green'))(100) # 
+plot(p224r63_2011$B2_sre, col=clg)
+
+#band n3
+clr <- colorRampPalette(c('dark red','red','pink'))(100) # 
+plot(p224r63_2011$B3_sre, col=clr)
+
+#band n4
+cln <- colorRampPalette(c('red','orange','yellow'))(100) # 
+plot(p224r63_2011$B4_sre, col=cln)
+
+dev.off()
+plotRGB(p224r63_2011, r=3, g=2, b=1, stretch="Lin")
+
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
+
+plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="Lin")
+
+plotRGB(p224r63_2011, r=3, g=2, b=4, stretch="Lin")
+
+#all bands in the same page 
+par(mfrow=c(2,2))
+plotRGB(p224r63_2011, r=3, g=2, b=1, stretch="Lin")
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
+plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="Lin")
+plotRGB(p224r63_2011, r=3, g=2, b=4, stretch="Lin")
+
+
