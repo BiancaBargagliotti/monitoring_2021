4a3ee59ecbb958f21e2d0ca4ed6f7afc22a82c28
BiancaBargagliotti <73693975+BiancaBargagliotti@users.noreply.github.com>
2021-01-11 10:05:07 +0100

    Create R__code_exam_simulation.r

---
+++ R__code_exam_simulation.r
@@ -0,0 +1,8 @@
+# R code for the exam
+
+# we need a library to read the data downloded from Copernicus
+# we use ncdf4 library
+install.packages("ncdf4")
+library(ncdf4)
+
+# set the working directory
