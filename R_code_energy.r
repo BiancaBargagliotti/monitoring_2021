ed2dd84707931799840daa1ba3c533be76da111c
BiancaBargagliotti <73693975+BiancaBargagliotti@users.noreply.github.com>
2021-01-16 16:59:17 +0100

    Create R_code_energy.r

---
+++ R_code_energy.r
@@ -0,0 +1,55 @@
+# R_code_energy.r
+
+# install package rasterdiv (has 3 packages on ndvi)
+install.packages("rasterdiv")
+library(rasterdiv)
+library(raster)
+
+#load data
+data(copNDVI)
+#plot data 
+plot(copNDVI)
+
+# reclassifyy daya, excluding some
+copNDVI <- reclassify(copNDVI, cbind(253:255, NA))
+plot(copNDVI)
+
+# install also rasterVis
+install.packages("rasterVis")
+library(rasterVis)
+
+levelplot(copNDVI)
+
+# change colours in the plot
+# yellow in the minimum part 
+clymin <- colorRampPalette(c('yellow','red','blue'))(100) #
+plot(copNDVI, col=clymin)
+
+# yellow in the medium part 
+clymed <- colorRampPalette(c('red','yellow','blue'))(100) # 
+plot(copNDVI, col=clymed)
+
+# yellow in the maximum part 
+clymax <- colorRampPalette(c('red','blue','yellow'))(100) #
+plot(copNDVI, col=clymax)
+
+# or
+
+clymax <- colorRampPalette(c('blue','red','yellow'))(100) #
+plot(copNDVI, col=clymax)
+
+# 2 maps in the same page 
+par(mfrow=c(1,2))
+clymed <- colorRampPalette(c('red','yellow','blue'))(100) # 
+plot(copNDVI, col=clymed)
+clymax <- colorRampPalette(c('blue','red','yellow'))(100) #
+plot(copNDVI, col=clymax)
+
+# zoom in a part of the map
+dev.off()
+#extension of the zoom
+ext <- c(0,20,35,55)
+#do the crop
+copNDVI_Italy <- crop(copNDVI, ext)
+#plot
+plot(copNDVI_Italy, col=clymax)
