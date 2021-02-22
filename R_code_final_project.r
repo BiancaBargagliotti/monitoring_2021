43b95f66c029ac991df11779e35ed1c4d0d0430b
BiancaBargagliotti <73693975+BiancaBargagliotti@users.noreply.github.com>
2021-02-22 18:30:38 +0100

    Update R_code_final_project.r

--- R_code_final_project.r
+++ R_code_final_project.r
@@ -1,22 +1,4 @@
-# R_code_final_project.r     https://volcano.si.edu/gvp_votw.cfm 
+# R_code_final_project.r     
 # setwd("/Users/yourname/lab/")
-setwd("/Users/bianca/Desktop/GCE&SDGs/Monitoring Ecosystems/lab/")
-library(readxl)
-eruptions <- read.table("GVP_Eruption_Results.csv", header=TRUE, sep=",",stringsAsFactors = FALSE)
-summary(eruptions)
-head(eruptions)
 
-library(ggplot2)
-ggplot(eruptions, aes(x=Latitude, y=Longitude)) + geom_point()
-
-
-install.packages("readxl")
-
-library("readxl")
-read.table("/Users/bianca/Desktop/GCE&SDGs/Monitoring Ecosystems/lab/GVP_Eruption_Results.csv")
-read_excel("/Users/bianca/Desktop/GCE&SDGs/Monitoring Ecosystems/lab/GVP_Eruption_Results.xls")
-
-library("readxl")
-read_excel("C:\\Users\\Ron\\Desktop\\product_list.xlsx")
-eruptions <- read_excel("GVP_Eruption_Results")
  
