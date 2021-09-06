1af830d846063abebf591c68b0186fbf757cd06f
BiancaBargagliotti <73693975+BiancaBargagliotti@users.noreply.github.com>
2021-09-06 14:27:09 +0200

    Update R_code_final_project.r

--- R_code_final_project.r
+++ R_code_final_project.r
@@ -1,20 +1,10 @@
 # R_code_final_project.r    
 
 
-# we need a library to read the data downloaded from Copernicus (https://land.copernicus.vgt.vito.be/PDF/portal/Application.html#Home)
-# we use ncdf4 library
-install.packages("ncdf4")
-library(ncdf4)
-# set the working directory
-setwd("/Users/bianca/Desktop/GCE&SDGs/Monitoring Ecosystems/lab/")
-library(raster)
+# I want to assess the consequences on vegetation of fires happened in the Amazon forest during the summer of 2019
+# In order to do this, I consider the NDVI, to asses the greenness of the area, and the FCover, to quantify the spatial extent of the vegetation before, during and after the fires of 2019.
 
-SWI21 <- raster("c_gls_SWI1km_202106021200_CEURO_SCATSAR_V1.0.1.nc")
-clSWI21 <- colorRampPalette(c("tomato", "gold", "navy"))(100) #
-plot(SWI21, col=clSWI21)
+# The data used are downloaded from Copernicus 
 
-SWI20 <- raster("c_gls_SWI1km_202006021200_CEURO_SCATSAR_V1.0.1.nc")
-plot(SWI20, col=clSWI21)
+# First, I set the working directory
 
-difSWI <- SWI21 - SWI20
-plot(difSWI)
